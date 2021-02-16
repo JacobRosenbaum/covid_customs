092f4c6516701f240c66fc287daeb680062d05b2
KendraKrosch <Kkrosch@dev-10.com>
2021-02-16 14:15:01 -0600

    adding materialEnum part 2

--- src/main/java/learn/covid_customs/models/Material.java
+++ src/main/java/learn/covid_customs/models/Material.java
@@ -1,2 +1,18 @@
-package learn.covid_customs.models;public enum Material {
+package learn.covid_customs.models;
+
+import lombok.*;
+
+
+public enum Material {
+    POLY_COT(1, "50% polyester and 50% cotton"),
+    POLYESTER(2, "polyester"),
+    COTTON(3, "Cotton");
+
+    @Getter private final int value;
+    @Getter private final String name;
+
+    Material(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
 }
