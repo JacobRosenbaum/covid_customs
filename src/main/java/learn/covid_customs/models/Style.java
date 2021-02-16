9b9720545c5c312d30ade1f9c17a6e34b0c42ee5
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2021-02-16 12:29:00 -0800

    add style enum:

--- src/main/java/learn/covid_customs/models/Style.java
+++ src/main/java/learn/covid_customs/models/Style.java
@@ -1,4 +1,15 @@
 package learn.covid_customs.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum Style {
+    ATHLETIC(1, "Athletic"),
+    OVER_EAR(2, "Over the Ear"),
+    WRAP(3, "Wrap");
+
+    @Getter private final int value;
+    @Getter private final String name;
+
 }
