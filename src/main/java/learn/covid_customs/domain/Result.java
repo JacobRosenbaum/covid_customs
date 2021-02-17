8618a4c815d07020da78dd21857508a2a6d18de0
KendraKrosch <Kkrosch@dev-10.com>
2021-02-16 18:34:45 -0600

    adding maskService

--- src/main/java/learn/covid_customs/domain/Result.java
+++ src/main/java/learn/covid_customs/domain/Result.java
@@ -1,21 +1,29 @@
 package learn.covid_customs.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 
-public class Result <T> {
+@EqualsAndHashCode
+public class Result<T> {
 
-    @Getter ArrayList<String> messages= new ArrayList<>();
-    @Getter ResultType type= ResultType.SUCCESS;
-    @Getter @Setter T payload;
+    @Getter
+    ArrayList<String> messages = new ArrayList<>();
 
-    public void addMessages(String message, ResultType type){
-        this.type= type;
+    @Getter
+    ResultType type = ResultType.SUCCESS;
+
+    @Getter @Setter
+    T payload;
+
+    public void addMessage(String message, ResultType type) {
+        this.type = type;
         messages.add(message);
     }
-    public boolean isSuccess(){
-        return type== ResultType.SUCCESS;
+
+    public boolean isSuccess() {
+        return type == ResultType.SUCCESS;
     }
 }
--- src/main/java/learn/covid_customs/models/Material.java
+++ src/main/java/learn/covid_customs/models/Material.java
@@ -1,18 +1,19 @@
 package learn.covid_customs.models;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 
+@AllArgsConstructor
 public enum Material {
     POLY_COT(1, "50% polyester and 50% cotton"),
     POLYESTER(2, "polyester"),
     COTTON(3, "Cotton");
 
-    @Getter private final int value;
-    @Getter private final String name;
+    @Getter
+    private final int value;
 
-    Material(int value, String name) {
-        this.value = value;
-        this.name = name;
-    }
+    @Getter
+    private final String name;
+
 }
