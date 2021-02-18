a847ce2ea4d1ccfaabda1479472ae84ed90d590b
KendraKrosch <Kkrosch@dev-10.com>
2021-02-18 16:04:08 -0600

    attempt at api

--- client/covid-custom-app/src/components/CovidAPI.tsx
+++ client/covid-custom-app/src/components/CovidAPI.tsx
@@ -1,4 +1,27 @@
+import { useState} from 'react';
+
 function CovidAPI(){
+
+	const [data, setData] = useState<any[]>([]);
+
+	function functionAPI(){
+		fetchingData().then(json => console.log(json))
+		.catch(console.log);
+	};
+	const fetchingData= async()=> await fetch("https://api.covidtracking.com/v1/us/daily.json")
+            .then(response => {
+				console.log(response.status);
+                if (response.status !== 200) {
+                    return Promise.reject("Agents fetch failed")
+				}
+                return response.json();
+			})
+			
+	functionAPI();
+
+
+            
+functionAPI();
     return(
         <>
         <h2>Covid Info</h2>
