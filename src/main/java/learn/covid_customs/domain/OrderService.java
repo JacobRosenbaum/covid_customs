fb4339d5bb40be489ade2c5ecf739ca1de1b71b7
KendraKrosch <Kkrosch@dev-10.com>
2021-02-18 13:37:55 -0600

    Service Order complete Tests still need to be implemented

--- src/main/java/learn/covid_customs/domain/OrderService.java
+++ src/main/java/learn/covid_customs/domain/OrderService.java
@@ -5,8 +5,13 @@ import learn.covid_customs.models.Order;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -27,11 +32,32 @@ public class OrderService {
 
     public Result<Order> add(Order order){
         Result<Order> result= validate(order);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (order.getOrderId() > 0) {
+            result.addMessage("OrderId cannot be set for adding a order.", ResultType.INVALID);
+            return result;
+        }
+        result.setPayload(repository.add(order));
         return result;
     }
 
     public Result<Order> update(Order order){
         Result<Order> result= validate(order);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (order.getOrderId() <= 0) {
+            result.addMessage("OrderId must be set for update a order.", ResultType.INVALID);
+            return result;
+        }
+        if (!repository.update(order)) {
+            String message = String.format("Mask Id %s not found.", order.getOrderId());
+            result.addMessage(message, ResultType.NOT_FOUND);
+            return result;
+        }
+        result.setPayload(order);
         return result;
     }
 
@@ -41,8 +67,22 @@ public class OrderService {
 
     public Result<Order> validate(Order order){
         Result<Order> result= new Result<>();
+        if (order == null) {
+            result.addMessage("Order cannot null.", ResultType.INVALID);
+            return result;
+        }
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Order>> violations = validator.validate(order);
+
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<Order> violation : violations) {
+                result.addMessage(violation.getMessage(), ResultType.INVALID);
+            }
+        }
         return result;
     }
+
 
     //May not need below
 //    private BigDecimal calculateTotal(Order order){
