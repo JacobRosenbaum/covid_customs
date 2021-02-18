3f52f7dda32479e46cfbeda467eb16448a3b337f
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2021-02-18 11:22:46 -0800

    update react images issue / orderController

---
+++ src/main/java/learn/covid_customs/controllers/OrderController.java
@@ -0,0 +1,79 @@
+package learn.covid_customs.controllers;
+
+import learn.covid_customs.domain.Result;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> findAll() {
+        return orderService.findAll();
+    }
+
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> findByOrderId(@PathVariable int orderId) {
+        Order order = orderService.findById(orderId);
+
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(order);
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Order> findById(@PathVariable int customerId) {
+        Order order = orderService.findByCustomerId(customerId);
+
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(order);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody @Valid Order order) {
+        Result<Order> result = orderService.add(order);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<Object> update(@PathVariable int orderId, @RequestBody @Valid Order order) {
+        if (order.getOrderId() != orderId) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Order> result = orderService.update(order);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int orderId) {
+        if (orderService.deleteById(orderId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
