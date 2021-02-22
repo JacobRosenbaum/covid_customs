c282f8e1add592cb110b3255e14ed02451bea7cb
KendraKrosch <Kkrosch@dev-10.com>
2021-02-22 15:43:36 -0600

    finished admin just need to implement security

--- sql/covid_customs_mask_data_inserts.sql
+++ sql/covid_customs_mask_data_inserts.sql
@@ -5,6 +5,8 @@ set sql_safe_updates = 0;
 delete from color;
 delete from order_mask;
 delete from mask;
+delete from user_account;
+delete from customer;
 set sql_safe_updates = 1;
 
     insert into mask values
@@ -34,3 +36,29 @@ set sql_safe_updates = 1;
     ( 9, 1, "Red"),
     ( 9, 5, "Blue"),
     ( 9, 8, "White");
+    
+--     insert into customer values
+-- 	(1, "Austin", "Shinnick", "austin@aol.com","763-464-6002","827 413rd Ave NW", "Minneapolis", "MN", 55414, "ADMIN"),
+--     (2, "SythJacob", "Rosenbaum", "jacob@yahoo.com","111-111-1111","that's no moon...", "Minneapolis", "MN", 55414, "USER"),
+--     (3, "Kendra", "Krosch", "kk@covidCustoms.com", "222-222-2222", "Colorful Colorado", "Minneapolis", "MN", 55414, "USER");
+--     
+-- insert into user_account values
+--     (1,"austin@aol.com", "password"),
+--     (2,"jacob@yahoo.com", "password"),
+--     (3,"kk@covidCustoms.com", "password");
+--     
+--     insert into orders values
+--     ( 1, 1, false, null),
+--     ( 2, 2, true, "2020-02-12"),
+--     ( 3, 3, true, "2020-02-15"),
+--     ( 4, 3, false, null);
+--     
+--     insert into order_mask values
+--     ( 1, 1, 3),
+--     ( 2, 1, 3),
+--     ( 2, 2, 4),
+--     ( 3, 3, 1),
+--     ( 4, 1, 2),
+--     ( 4, 2, 3),
+--     ( 4, 3, 1); 
+    
