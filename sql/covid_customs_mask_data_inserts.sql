74703675ebc4b53ee01ebf9b222cb852c37a4d91
Austin Shinnick <austin.shinnick@gmail.com>
2021-02-22 14:11:27 -0600

    customer account prelim commit

--- sql/covid_customs_mask_data_inserts.sql
+++ sql/covid_customs_mask_data_inserts.sql
@@ -34,3 +34,32 @@ set sql_safe_updates = 1;
     ( 9, 1, "Red"),
     ( 9, 5, "Blue"),
     ( 9, 8, "White");
+    
+    --   added below for testing purposes ---------------------------------------------------------------------------------------------------------------------------------
+
+
+
+ --    insert into user_account values
+--     (1, 'austin@aol.com', 'Password1234!');
+--     select * from user_account;
+    
+     insert into customer values
+	(1, "Austin", "Shinnick", "austin@aol.com","763-464-6002","827 413rd Ave NW", "ADMIN"),
+    (2, "SythJacob", "Rosenbaum", "jacob@yahoo.com","111-111-1111","that's no moon...", "USER"),
+    (3, "Kendra", "Krosch", "kk@covidCustoms.com", "222-222-2222", "Colorful Colorado", "USER");
+    select * from customer;
+    
+    insert into orders values
+    ( 1, 1, false, null),
+    ( 2, 2, true, "2020-02-12"),
+    ( 3, 3, true, "2020-02-15"),
+    ( 4, 3, false, null);
+    
+    insert into order_mask values
+    ( 1, 1, 3),
+    ( 2, 1, 3),
+    ( 2, 2, 4),
+    ( 3, 3, 1),
+    ( 4, 1, 2),
+    ( 4, 2, 3),
+    ( 4, 3, 1);
